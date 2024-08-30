e491a581ef84605cc0d4802b8d3c112dacda64d5
Shaun Fox <shaunrfox@gmail.com>
2024-08-29 20:37:34 -0500

    Ported over files from non-vite app. Seems to be working.

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link, useLocation } from "@remix-run/react";
+import FoxLogo from "../Foxes/FoxLogo";
+import Rule from "../Rule";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "/app/components/Footer/footer.css",
+    },
+  ];
+};
+
+export function Copyright() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <p className="copyright">
+      Copyright &copy; {currentYear} &ndash; Shaun Fox
+    </p>
+  );
+}
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer>
+      <Rule thickness={4} />
+      <div className="footer-content">
+        <FoxLogo />
+        <div className="footer-links">
+          <Link to="mailto:hello@okshaun.com">Mail</Link>
+          <Link to="https://www.linkedin.com/in/shaunrfox" target="_blank">
+            LinkedIn
+          </Link>
+          <Link to="http://codepen.io/shaunrfox/" target="_blank">
+            CodePen
+          </Link>
+          <Link to="https://github.com/shaunrfox" target="_blank">
+            GitHub
+          </Link>
+        </div>
+        <Copyright />
+      </div>
+    </footer>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,30 +6,62 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import Header, { links as headerLinks } from "./components/Header";
+import Footer, { links as footerLinks } from "./components/Footer/Footer";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "/app/styles/global.css",
+    },
+    {
+      rel: "icon",
+      href: "/assets/favicon.ico",
+      type: "image/x-icon",
+    },
+    {
+      rel: "shortcut icon",
+      href: "/assets/favicon.ico",
+      type: "image/x-icon",
+    },
+    ...headerLinks(),
+    ...footerLinks(),
+  ];
+};
+
+export const meta: MetaFunction = () => {
+  return [{ title: "okshaun.com" }];
+};
+
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="I'm an experienced Product Designer living in Austin, TX, with my beautiful wife and daughters."
+        />
+        <meta
+          name="keywords"
+          content="Shaun Fox, Shaun, Sean, Shawn, Fox, Design, Austin, Designer, Principal Product Designer, Product Designer, Lead UX Designer, UX Designer, Portfolio, Web Design, design technologist"
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        {/* <div className="breakpoint-tester"></div> */}
+        <Header />
+        <main className="container">
+          <Outlet />
+        </main>
+        <Footer />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
-}
-
-export function HydrateFallback() {
-  return <p>Loading...</p>;
 }
